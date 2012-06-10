c04d55d640641fbd52e1824f7f827efea9aed4bd
kantanomo <thekantanomo@live.com>
2012-06-10 03:50:59 -0500

    Used With Weapon Testing

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,29 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -6,14 +6,21 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
+using H2Memory_class;
 namespace H2Memory_Tests
 {
     public partial class Form1 : Form
     {
+        H2Memory H2 = new H2Memory(H2Type.Halo2Vista, true);
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (H2.GameStateCheck() == GameState.ingame)
+                new Player(H2, 0).WeaponOut().AmmoLeft = 9999;
         }
     }
 }
