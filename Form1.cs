4dcb42cf8a5196285419452efc982472c8b5016c
kantanomo <thekantanomo@live.com>
2012-07-15 13:05:47 -0500

    More Tests

--- Form1.cs
+++ Form1.cs
@@ -15,24 +15,40 @@ namespace H2Memory_Tests
         public Form1()
         {
             InitializeComponent();
+            //OpCode.PatchChecks(H2);
+            //OpCode.PatchRank(H2);
+            //OpCode.PatchSpeed(H2);
+            //OpCode.PatchAmmo(H2);
+            //OpCode.PathGrenades(H2);
+            //foreach (int i in DynamicObjTable.GetAllWeapons(H2))
+            //{
+            //    H2.H2Mem.WriteInt(false, i + 0x22C, 2000);
+            //    H2.H2Mem.WriteInt(false, i + 0x22A, 2000);
+            //    H2.H2Mem.WriteFloat(false, i + 0x184, 10);
+            //}
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if (H2.GameStateCheck() == GameState.ingame)
+            if (H2.GameStateCheck() == GameState.ingame && H2.EngineCheck() == EngineType.Multiplayer)
             {
-                Player P = new Player();
-                WeaponBase Wb = new WeaponBase();
-                for (int i = 0; i < H2.GetPlayerCount(); i++)
+                foreach(int i in DynamicObjTable.GetWeaponSet(H2, Weapon.flag))
                 {
-                    P = new Player(H2, i);
-                    Wb = P.WeaponOut();
-                    if (Wb.WeaponClass != Weapon.None)
-                    {
-                        Wb.WeaponHeat = -10;
-                        Wb.BatteryPower = -10;
-                        Wb.AmmoLeft = 9999;
-                    }
+                    float Xtemp = H2.H2Mem.ReadFloat(false, i + 0x88);
+                    float Ytemp = H2.H2Mem.ReadFloat(false, i + 0x8C);
+                    if (Xtemp != Math.Abs(Xtemp))
+                        H2.H2Mem.WriteFloat(false, i + 0x88, Xtemp - 1f);
+                    if (Xtemp == Math.Abs(Xtemp))
+                        H2.H2Mem.WriteFloat(false, i + 0x88, Xtemp + 1f);
+                    if (Ytemp != Math.Abs(Ytemp))
+                        H2.H2Mem.WriteFloat(false, i + 0x8C, Ytemp - 1f);
+                    if (Ytemp == Math.Abs(Ytemp))
+                        H2.H2Mem.WriteFloat(false, i + 0x8C, Ytemp + 1f);
+                    H2.H2Mem.WriteFloat(false, i + 0x90, -20);
                 }
             }
         }
